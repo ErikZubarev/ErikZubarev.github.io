48666ebb128d03e5568bf269a6f0d3e192fd697a
Erik Zubarev <erikzubarev543@gmail.com>
2025-02-16 16:18:24 +0100

    PHP removed

---
+++ assets/js/sendmail.js
@@ -0,0 +1,17 @@
+document.getElementById('contactform').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+
+    fetch('/.netlify/functions/send-email', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+});
