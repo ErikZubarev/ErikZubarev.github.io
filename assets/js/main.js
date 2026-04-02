bb9a8e65db95b0306b5feca028aad3dce2bfb8be
Erik Zubarev <45092431+ErikZubarev@users.noreply.github.com>
2026-04-02 13:42:02 +0200

    Fixed gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -186,5 +186,111 @@
 
 				}
 			});
+	
+	
+	// Gallery.
+		$('.gallery')
+			.on('click', 'a', function(event) {
+			var $a = $(this),
+				$gallery = $a.parents('.gallery'),
+				$modal = $gallery.children('.modal'),
+				href = $a.attr('href');
 
+			// Only allow certain types
+			if (!href.match(/\.(jpg|gif|png|mp4)$/i))
+				return;
+
+			event.preventDefault();
+			event.stopPropagation();
+
+			if ($modal[0]._locked) return;
+			$modal[0]._locked = true;
+
+			// Clear previous content
+			var $inner = $modal.find('.inner');
+			$inner.empty();
+
+			if (href.match(/\.mp4$/i)) {
+				// Create video
+				var $video = $('<video controls autoplay />');
+				$video.attr('src', href);
+				$inner.append($video);
+
+				// When video is ready, mark loaded
+				$video.on('loadeddata', function() {
+				$modal.addClass('loaded');
+				});
+			} else {
+				// Create image
+				var $img = $('<img />');
+				$img.attr('src', href);
+				$inner.append($img);
+
+				$img.on('load', function() {
+				$modal.addClass('loaded');
+				});
+			}
+
+			$modal.addClass('visible').focus();
+
+			setTimeout(function() {
+				$modal[0]._locked = false;
+			}, 600);
+			})
+			
+			.on('click', '.modal', function(event) {
+			var $modal = $(this),
+				$inner = $modal.find('.inner');
+
+			if ($modal[0]._locked) return;
+			if (!$modal.hasClass('visible')) return;
+
+			event.stopPropagation();
+			$modal[0]._locked = true;
+
+			$modal.removeClass('loaded');
+
+			setTimeout(function() {
+				$modal.removeClass('visible');
+				setTimeout(function() {
+				$inner.empty(); // clear both img or video
+				$modal[0]._locked = false;
+				$body.focus();
+				}, 475);
+			}, 125);
+			})
+			.on('keypress', '.modal', function(event) {
+
+				var $modal = $(this);
+
+				// Escape? Hide modal.
+					if (event.keyCode == 27)
+						$modal.trigger('click');
+
+			})
+			.on('mouseup mousedown mousemove', '.modal', function(event) {
+
+				// Stop propagation.
+					event.stopPropagation();
+
+			})
+			.prepend('<div class="modal" tabIndex="-1"><div class="inner"></div></div>')
+				.find('img')
+					.on('load', function(event) {
+
+						var $modalImg = $(this),
+							$modal = $modalImg.parents('.modal');
+
+						setTimeout(function() {
+
+							// No longer visible? Bail.
+								if (!$modal.hasClass('visible'))
+									return;
+
+							// Set loaded.
+								$modal.addClass('loaded');
+
+						}, 275);
+
+					});
 })(jQuery);
